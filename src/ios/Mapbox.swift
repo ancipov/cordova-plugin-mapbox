794374edcde4d8d4cdd01400fd856e168606674d
Shitikov Alexandr <ashitikov@exadel.com>
2022-02-09 15:28:47 +0200

    Added new functionality
    - removeAllMarkers
    - tap on map callback
    - new params when showing map

--- src/ios/Mapbox.swift
+++ src/ios/Mapbox.swift
@@ -1,4 +1,3 @@
-//import Cordova
 import MapboxMaps
 
 @objc(Mapbox) class Mapbox : CDVPlugin {
@@ -6,17 +5,19 @@ import MapboxMaps
     var mapView: MapView!
     var tileStore: TileStore?
     var saveButton: UIButton!
+    var mapTouchCallbackId: String?
+    var pointAnnotationManager: PointAnnotationManager!
     
     private lazy var offlineManager: OfflineManager = {
         return OfflineManager(resourceOptions: mapInitOptions.resourceOptions)
     }()
     
     private lazy var mapInitOptions: MapInitOptions = {
-        MapInitOptions(cameraOptions: CameraOptions(center: tokyoCoord, zoom: maxZoom),
+        MapInitOptions(cameraOptions: CameraOptions(center: defaultCoordinates, zoom: maxZoom),
                        styleURI: .outdoors)
     }()
     
-    private let tokyoCoord = CLLocationCoordinate2D(latitude: 34.043492, longitude: -118.258644)
+    private let defaultCoordinates = CLLocationCoordinate2D(latitude: 34.043492, longitude: -118.258644)
     private let maxZoom: CGFloat = 12
     
     // MARK: - Map
@@ -25,9 +26,24 @@ import MapboxMaps
     func addMarkerToCenter(command: CDVInvokedUrlCommand) {
         let center = mapView.mapboxMap.cameraState.center
         let params = ["lat": center.latitude, "lon": center.longitude]
-        putMarkersOnTheMap(annotations: [params])
-        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
-        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+        putMarkersOnTheMap(annotations: [params], id: command.callbackId)
+    }
+    
+    @objc(addMapTouchCallback:)
+    func addMapTouchCallback(command: CDVInvokedUrlCommand) {
+        self.mapTouchCallbackId = command.callbackId
+    }
+    
+    @objc func mapDidTouch(gesture: UIGestureRecognizer) {
+        if mapTouchCallbackId != nil && gesture.state == .ended {
+            let point = gesture.location(in: mapView)
+            let coordinates = mapView.mapboxMap.coordinate(for: point)
+            print(coordinates)
+            let params = ["lat" : coordinates.latitude, "lon": coordinates.longitude]
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: params)
+            pluginResult?.setKeepCallbackAs(true)
+            self.commandDelegate!.send(pluginResult, callbackId: self.mapTouchCallbackId)
+        }
     }
     
     @objc(showMap:)
@@ -40,14 +56,37 @@ import MapboxMaps
         let right = margins["right"] as? Int ?? 0
         let top = margins["top"] as? Int ?? 0
         let bottom =  margins["bottom"] as? Int ?? 0
+        let height = margins["height"] as? Int
         
         let webViewFrame = self.webView.frame
-        let mapFrame = CGRect(x: left, y: top, width: Int(webViewFrame.size.width) - left - right, height: Int(webViewFrame.size.height) - top - bottom)
+        var mapFrame: CGRect!
+        
+        //  when set height ignoring bottom margin
+        if let height = height {
+            mapFrame = CGRect(x: left, y: top, width: Int(webViewFrame.size.width) - left - right, height: height)
+        } else {
+            mapFrame = CGRect(x: left, y: top, width: Int(webViewFrame.size.width) - left - right, height: Int(webViewFrame.size.height) - top - bottom)
+        }
         
         mapView = MapView(frame:mapFrame, mapInitOptions: myMapInitOptions)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         mapView.tintColor = .gray
+        if let center = args["center"] as? Dictionary<String, Any>, let lat = center["lat"] as? Double, let lon = center["lon"] as? Double {
+            let zoom = args["zoom"] as? CGFloat ?? maxZoom
+            let centerCoordinate = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+            mapView.mapboxMap.setCamera(to: CameraOptions(center: centerCoordinate, padding: UIEdgeInsets.zero, anchor: nil, zoom: zoom, bearing: nil, pitch: nil))
+        }
+        
         self.webView.addSubview(mapView)
+        mapView.gestures.singleTapGestureRecognizer.addTarget(self, action: #selector(mapDidTouch(gesture:)))
+        pointAnnotationManager = self.mapView.annotations.makePointAnnotationManager()
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+    }
+    
+    @objc(removeAllMarkers:)
+    func removeAllMarkers(command: CDVInvokedUrlCommand) {
+        pointAnnotationManager.annotations.removeAll()
         let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
         self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
     }
@@ -57,13 +96,11 @@ import MapboxMaps
         guard let annotations: Array = command.arguments[0] as? Array<Any> else {
             return
         }
-        putMarkersOnTheMap(annotations: annotations)
-        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
-        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+        putMarkersOnTheMap(annotations: annotations, id: command.callbackId)
         
     }
     
-    private func putMarkersOnTheMap(annotations: [Any]) {
+    private func putMarkersOnTheMap(annotations: [Any], id: String) {
         self.commandDelegate.run {
             var pointAnnotations = [PointAnnotation]()
             annotations.forEach { item in
@@ -79,8 +116,10 @@ import MapboxMaps
                     pointAnnotations.append(annotation)
                 }
             }
-            let pointAnnotationManager = self.mapView.annotations.makePointAnnotationManager()
-            pointAnnotationManager.annotations = pointAnnotations
+            self.pointAnnotationManager.annotations.append(contentsOf: pointAnnotations)
+            let annotationsIds = pointAnnotations.map {$0.id}
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: annotationsIds)
+            self.commandDelegate!.send(pluginResult, callbackId:id)
         }
     }
     
--- www/Mapbox.js
+++ www/Mapbox.js
@@ -1,20 +1,29 @@
 var exec = require("cordova/exec");
 
 module.exports = {
-showMap: function (options, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "Mapbox", "showMap", [options]);
-},
+    showMap: function (options, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Mapbox", "showMap", [options]);
+    },
     
-addMarkers: function (options, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "Mapbox", "addMarkers", [options]);
-},
+    addMarkers: function (options, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Mapbox", "addMarkers", [options]);
+    },
     
-saveTile: function (successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "Mapbox", "saveTile", []);
-},
+    saveTile: function (successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Mapbox", "saveTile", []);
+    },
     
-addMarkerToCenter: function (successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "Mapbox", "addMarkerToCenter", []);
-}
+    addMarkerToCenter: function (successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Mapbox", "addMarkerToCenter", []);
+    },
+    
+    addMapTouchCallback: function (callback) {
+        cordova.exec(callback, null, "Mapbox", "addMapTouchCallback", []);
+    },
+    
+    removeAllMarkers: function (successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Mapbox", "removeAllMarkers", []);
+    }
+    
     
 };
